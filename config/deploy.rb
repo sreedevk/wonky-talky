a8a4dff7481ec49e3ea1059cc8266574ac226cca
sreedev <sreedev@icloud.com>
2020-02-02 01:15:44 +0530

    deployment changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,5 +7,6 @@ set :scm, :git
 set :rails_env, :production
 append :linked_files, 'config/database.yml', 'config/secrets.yml'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+set :passenger_restart_with_touch, true
 set :keep_releases, 5
 set :pty, false
